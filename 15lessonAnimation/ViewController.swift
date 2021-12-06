8e021eef16ecd2046ba22ce7a0ef17ea06c9c644
vladputsikovich <vladputsikovich@gmail.com>
2021-12-06 23:43:58 +0300

    Add animation 1 task

--- 15lessonAnimation/ViewController.swift
+++ 15lessonAnimation/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        _ = addView(x: 20, y: 20, width: 100, height: 100, option: UIView.KeyframeAnimationOptions.calculationModeDiscrete)
+        
+        _ = addView(x: 20, y: 130, width: 100, height: 100, option: UIView.KeyframeAnimationOptions.allowUserInteraction)
+        
+        _ = addView(x: 20, y: 240, width: 100, height: 100, option: UIView.KeyframeAnimationOptions.allowUserInteraction)
+        
+        _ = addView(x: 20, y: 350, width: 100, height: 100, option: UIView.KeyframeAnimationOptions.allowUserInteraction)
+        
+        _ = addView(x: 20, y: 460, width: 100, height: 100, option: UIView.KeyframeAnimationOptions.allowUserInteraction)
     }
-
-
+    
+    func addView(x: Int, y: Int, width: Int, height: Int, option: UIView.KeyframeAnimationOptions) -> UIView {
+        let view = UIView(frame: CGRect(x: x, y: y, width: width, height: height))
+        view.backgroundColor = UIColor.blue
+        self.view.addSubview(view)
+        
+        UIView.animateKeyframes(
+            withDuration: 5,
+            delay: 1,
+            options: option) {
+                view.center = CGPoint(x: self.view.bounds.width - view.frame.width / 2, y: CGFloat(y) + view.frame.width / 2)
+                view.backgroundColor = UIColor(
+                    red: CGFloat.random(in: 0...1),
+                    green: CGFloat.random(in: 0...1),
+                    blue: CGFloat.random(in: 0...1),
+                    alpha: 1.0
+                )
+        } completion: { use in
+            print(use)
+        }
+        return view
+    }
 }
 
